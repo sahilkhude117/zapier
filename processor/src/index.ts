042d3c946cb499c7f1102c7eb0662a14733169a5
sahilkhude117 <sahilkhude11@gmail.com>
2025-02-13 21:53:06 +0530

    kafka logic

---
+++ processor/src/index.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+import { Kafka } from "kafkajs";
+
+const TOPIC_NAME = 'zap-events'
+
+const prisma = new PrismaClient();
+
+const kafka = new Kafka({
+    clientId: 'outbox-processor',
+    brokers: ['localhost:9092']
+})
+
+async function main() {
+    const producer = kafka.producer();
+    await producer.connect();
+    
+    while(1){
+        const pendingRows = await prisma.zapRunOutbox.findMany({
+            where:{},
+            take: 10
+        })
+
+        producer.send({
+            topic: TOPIC_NAME,
+            messages: pendingRows.map(r => ({
+                value: r.zapRunId
+            }))
+        })
+
+        await prisma.zapRunOutbox.deleteMany({
+            where: {
+                id: {
+                    in: pendingRows.map(x => x.id)
+                }
+            }
+        })
+    }
+}
+
+main()
